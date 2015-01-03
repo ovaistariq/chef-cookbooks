052205b270646e47205974f8a588be99db52c046
Ovais Tariq <ovaistariq@gmail.com>
2015-01-02 20:51:48 -0500

    Add mysql-chef_gem cookbook as a dependency

--- percona-tools/metadata.rb
+++ percona-tools/metadata.rb
@@ -11,7 +11,7 @@ recipe "percona-tools::percona_repo",       "Sets up the Percona package reposit
 recipe "percona-tools::percona_tools",      "Installs the Percona Toolkit and Percona XtraBackup softwares"
 
 depends "yum", "~> 3.0"
-depends "mysql", "~> 6.0"
+depends "mysql-chef_gem", "~> 0.0"
 depends "database", "~> 2.1"
 depends "openssl"
 
